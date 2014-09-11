528a61c4a6c84384ea159c70e32cf0ad61ce414f
zimme <zimme@users.noreply.github.com>
2014-09-11 14:22:46 +0200

    Version bump

--- package.js
+++ package.js
@@ -2,12 +2,13 @@ Package.describe({
   git: 'https://github.com/zimme/meteor-bootstrap-growl.git',
   name: 'zimme:bootstrap-growl',
   summary: 'Turns standard Bootstrap alerts into "Growl-like" notifications.',
-  version: '0.0.1',
+  version: '0.1.0',
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('0.9.0');
+
   api.use('jquery', 'client');
+
   api.addFiles('bootstrap-growl/bootstrap-growl.js', 'client');
 });
-
